9d16079c9eb6f77aad3953cdb3ef143c0cd94b04
Coby Tamayo <ctamayo@sitecrafting.com>
2020-12-17 18:37:56 -0800

    Test env bootstrap tooling

--- scripts/init-cumulus.sh
+++ scripts/init-cumulus.sh
@@ -31,6 +31,39 @@ function main() {
   wp option set cumulus_cloud_name "$CUMULUS_CLOUD_NAME"
   wp option set cumulus_api_key "$CUMULUS_API_KEY"
   wp option set cumulus_api_secret "$CUMULUS_API_SECRET"
+  wp option set cumulus_folder "$CUMULUS_FOLDER"
+
+  # set up home page
+  home_id=$(wp post create --post_type=page --post_title="Home" --post_status=publish --porcelain)
+  wp option set page_on_front $home_id
+  wp option set show_on_front page
+  wp rewrite flush
+
+  grasshopper=$(wp media import dev/img/grasshopper.jpg --porcelain \
+    --title="Grasshopper" \
+    --alt="Grasshopper by Coby")
+
+  spider=$(wp media import dev/img/spider.jpg --porcelain \
+    --title="Spider" \
+    --alt="Spider by Coby")
+
+  heron=$(wp media import dev/img/heron.jpg --porcelain \
+    --title="Heron" \
+    --alt="Heron by Coby")
+
+  wp post meta set $home_id test_image_1 $grasshopper
+  wp post meta set $home_id test_image_2 $spider
+  wp post meta set $home_id test_image_3 $heron
+
+  wp media import dev/img/sculpture.jpg --title="Sculpture" \
+    --caption="Photo Credit: Chris Robinson https://www.flickr.com/people/11829757@N02" \
+    --alt="A sculpture at the entrance to the Palace of Versaille"
+
+  wp plugin deactivate cumulus
+  wp media import dev/img/orca.jpg --title="Orca" \
+    --alt="Orca by Coby" \
+    --desc="This image does not have cumulus_image metadata because it was uploaded while cumulus was disabled."
+  wp plugin activate cumulus
 }
 
 
@@ -63,6 +96,11 @@ case $key in
     ;;
   -s|--secret)
     CUMULUS_API_SECRET="$2"
+    shift # past flag
+    shift # past value
+    ;;
+  -f|--folder)
+    CUMULUS_FOLDER"$2"
     shift # past flag
     shift # past value
     ;;
