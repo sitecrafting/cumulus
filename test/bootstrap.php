3603dd3067e4fdf5d98b47b9a475e2866a2d65db
Coby Tamayo <ctamayo@sitecrafting.com>
2020-12-17 17:08:50 -0800

    get some dummy PHPUnit tests working

---
+++ test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '../../vendor/autoload.php';
+
+if (is_dir(__DIR__ . '/wp-tests-lib')) {
+  require_once __DIR__ . '/wp-tests-lib/includes/functions.php';
+  require_once __DIR__ . '/wp-tests-lib/includes/bootstrap.php';
+}
---
+++ test/integration/ApiTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Tests for the core API functions
+ *
+ * @copyright 2020 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace Cumulus\Integration;
+
+/**
+ * Stuff
+ */
+class ApiTest extends IntegrationTest {
+  public function testThingy() {
+    $this->assertTrue(true);
+  }
+}
