8d1d972fdae90ef03749a642b7b41d29b37a0154
Coby Tamayo <ctamayo@sitecrafting.com>
2020-12-18 20:10:01 -0800

    bump version

--- cumulus.php
+++ cumulus.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Cumulus: Cloudinary Image Crops
  * Description: Serve your custom image crops from Cloudinary Image CDN
- * Version: 0.0.2
+ * Version: 0.2.0
  * Author: SiteCrafting, Inc.
  * Author URI: https://www.sitecrafting.com
  *
