b9c4de636611f76a2c4f756c3306e5db6894e4ef
Sean Dean <sdean@sitecrafting.com>
2022-11-30 16:02:34 -0800

    Update request for composer autoload file to look in site vendor directory first

--- cumulus.php
+++ cumulus.php
@@ -18,99 +18,99 @@
  * - Nicer styles (nice-to-have)
  */
 
-
-require_once __DIR__ . '/vendor/autoload.php';
+if (file_exists(ABSPATH . '/../vendor/autoload.php')) {
+    require_once ABSPATH . '/../vendor/autoload.php';
+} else if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require_once __DIR__ . '/vendor/autoload.php';
+} else {
+    exit('Missing composer autoload file');
+}
 
 require_once __DIR__ . '/api.php';
 
 use Cloudinary\Configuration\Configuration;
-
 use SiteCrafting\Cumulus;
 use SiteCrafting\Cumulus\WpCli;
-
 
 define('CUMULUS_JS_DIR', __DIR__ . '/dist/js/');
 define('CUMULUS_CSS_DIR', __DIR__ . '/dist/css/');
 define('CUMULUS_VIEW_DIR', __DIR__ . '/views/');
-
 
 /**
  * Configure global Cloudinary settings.
  *
  * TODO implement a settings panel where admins can do this on their own.
  */
-add_action('init', function() {
-  $settings = Cumulus\settings();
+add_action('init', function () {
+    $settings = Cumulus\settings();
 
-  Configuration::instance([
-    'account'      => [
-      'cloud_name' => $settings['cloud_name'],
-      'api_key'    => $settings['api_key'],
-      'api_secret' => $settings['api_secret'],
-    ],
-    'url'          => [
-      'secure'     => true,
-    ],
-  ]);
+    Configuration::instance([
+        'account' => [
+            'cloud_name' => $settings['cloud_name'],
+            'api_key'    => $settings['api_key'],
+            'api_secret' => $settings['api_secret'],
+        ],
+        'url'     => [
+            'secure' => true,
+        ],
+    ]);
 });
-
 
 /**
  * Configure fallbacks for global settings.
  * If option does not exist in the database, look for a constant or an
  * environment variable, in that order.
  */
-add_filter('cumulus/settings', function($settings = []) {
-  $settings = wp_parse_args(
-    $settings,
-    apply_filters('cumulus/settings/defaults', [])
-  );
+add_filter('cumulus/settings', function ($settings = []) {
+    $settings = wp_parse_args(
+        $settings,
+        apply_filters('cumulus/settings/defaults', [])
+    );
 
-  if (empty($settings['cloud_name']) && defined('CUMULUS_CLOUD_NAME')) {
-    $settings['cloud_name'] = CUMULUS_CLOUD_NAME;
-  }
-  if (empty($settings['cloud_name']) && !empty($_ENV['CUMULUS_CLOUD_NAME'])) {
-    $settings['cloud_name'] = $_ENV['CUMULUS_CLOUD_NAME'];
-  }
+    if (empty($settings['cloud_name']) && defined('CUMULUS_CLOUD_NAME')) {
+        $settings['cloud_name'] = CUMULUS_CLOUD_NAME;
+    }
+    if (empty($settings['cloud_name']) && !empty($_ENV['CUMULUS_CLOUD_NAME'])) {
+        $settings['cloud_name'] = $_ENV['CUMULUS_CLOUD_NAME'];
+    }
 
-  if (empty($settings['api_key']) && defined('CUMULUS_API_KEY')) {
-    $settings['api_key'] = CUMULUS_API_KEY;
-  }
-  if (empty($settings['api_key']) && !empty($_ENV['CUMULUS_API_KEY'])) {
-    $settings['api_key'] = $_ENV['CUMULUS_API_KEY'];
-  }
+    if (empty($settings['api_key']) && defined('CUMULUS_API_KEY')) {
+        $settings['api_key'] = CUMULUS_API_KEY;
+    }
+    if (empty($settings['api_key']) && !empty($_ENV['CUMULUS_API_KEY'])) {
+        $settings['api_key'] = $_ENV['CUMULUS_API_KEY'];
+    }
 
-  if (empty($settings['api_secret']) && defined('CUMULUS_API_SECRET')) {
-    $settings['api_secret'] = CUMULUS_API_SECRET;
-  }
-  if (empty($settings['api_secret']) && !empty($_ENV['CUMULUS_API_SECRET'])) {
-    $settings['api_secret'] = $_ENV['CUMULUS_API_SECRET'];
-  }
+    if (empty($settings['api_secret']) && defined('CUMULUS_API_SECRET')) {
+        $settings['api_secret'] = CUMULUS_API_SECRET;
+    }
+    if (empty($settings['api_secret']) && !empty($_ENV['CUMULUS_API_SECRET'])) {
+        $settings['api_secret'] = $_ENV['CUMULUS_API_SECRET'];
+    }
 
-  if (empty($settings['folder']) && defined('CUMULUS_FOLDER')) {
-    $settings['folder'] = CUMULUS_API_SECRET;
-  }
-  if (empty($settings['folder']) && !empty($_ENV['CUMULUS_FOLDER'])) {
-    $settings['folder'] = $_ENV['CUMULUS_FOLDER'];
-  }
+    if (empty($settings['folder']) && defined('CUMULUS_FOLDER')) {
+        $settings['folder'] = CUMULUS_API_SECRET;
+    }
+    if (empty($settings['folder']) && !empty($_ENV['CUMULUS_FOLDER'])) {
+        $settings['folder'] = $_ENV['CUMULUS_FOLDER'];
+    }
 
-  return $settings;
+    return $settings;
 });
 
-add_filter('cumulus/settings/defaults', function() {
-  // Load Cumulus options from the database once and cache them for the
-  // request lifetime.
-  static $settings;
-  $settings = $settings ?? [
-    'cloud_name' => get_option('cumulus_cloud_name'),
-    'api_key'    => get_option('cumulus_api_key'),
-    'api_secret' => get_option('cumulus_api_secret'),
-    'folder'     => get_option('cumulus_folder'),
-  ];
+add_filter('cumulus/settings/defaults', function () {
+    // Load Cumulus options from the database once and cache them for the
+    // request lifetime.
+    static $settings;
+    $settings = $settings ?? [
+        'cloud_name' => get_option('cumulus_cloud_name'),
+        'api_key'    => get_option('cumulus_api_key'),
+        'api_secret' => get_option('cumulus_api_secret'),
+        'folder'     => get_option('cumulus_folder'),
+    ];
 
-  return $settings;
+    return $settings;
 });
-
 
 /**
  * The main event. Hook into the attachment src for a given size and
@@ -123,248 +123,246 @@ add_filter('cumulus/settings/defaults', function() {
  *
  * @see https://developer.wordpress.org/reference/functions/add_image_size/
  */
-add_filter('wp_get_attachment_image_src', function($src, $id, $size) {
-  if (!$src) {
+add_filter('wp_get_attachment_image_src', function ($src, $id, $size) {
+    if (!$src) {
+        return $src;
+    }
+
+    // Persist a mapping of attachment IDs to crop URLs, to save
+    // database calls
+    static $configsById;
+    if (!isset($configsById)) {
+        $configsById = [];
+    }
+
+    if (!isset($configsById[$id])) {
+        // Remember sizes for this attachment, if found.
+        $configsById[$id] = get_post_meta($id, 'cumulus_image', true) ?: [];
+    }
+
+    // Handle sizes expressed as [$width, $height] arrays.
+    if (is_array($size)) {
+        $size = sprintf('%sx%s', $size[0], $size[1]);
+    }
+
+    // Render the customized crop src, if there is one for this size.
+    $src[0] = $configsById[$id]['urls_by_size'][$size] ?? $src[0];
+    // TODO remove
+    $src[0] = $configsById[$id]['sizes'][$size] ?? $src[0];
+
     return $src;
-  }
-
-  // Persist a mapping of attachment IDs to crop URLs, to save
-  // database calls
-  static $configsById;
-  if (!isset($configsById)) {
-    $configsById = [];
-  }
-
-  if (!isset($configsById[$id])) {
-    // Remember sizes for this attachment, if found.
-    $configsById[$id] = get_post_meta($id, 'cumulus_image', true) ?: [];
-  }
-
-  // Handle sizes expressed as [$width, $height] arrays.
-  if (is_array($size)) {
-    $size = sprintf('%sx%s', $size[0], $size[1]);
-  }
-
-  // Render the customized crop src, if there is one for this size.
-  $src[0] = $configsById[$id]['urls_by_size'][$size] ?? $src[0];
-  // TODO remove
-  $src[0] = $configsById[$id]['sizes'][$size] ?? $src[0];
-
-  return $src;
 }, 10, 3);
 
 /*
  * Handle Retina Device Pixel Ratios automatically.
  */
-add_filter('wp_calculate_image_srcset', function($sources, $size, $src, $meta, $id) {
-  $img_data = $meta['cumulus_image'] ?? [];
-  if (empty($img_data)) {
-    return $sources;
-  }
+add_filter('wp_calculate_image_srcset', function ($sources, $size, $src, $meta, $id) {
+    $img_data = $meta['cumulus_image'] ?? [];
+    if (empty($img_data)) {
+        return $sources;
+    }
 
-  $ratios = apply_filters('cumulus/retina_dprs', [1, 2]);
-  if (empty($ratios)) {
-    return $sources;
-  }
+    $ratios = apply_filters('cumulus/retina_dprs', [1, 2]);
+    if (empty($ratios)) {
+        return $sources;
+    }
 
-  $cloud = Cumulus\cloud_name();
-  $size  = [
-    'width'  => $size[0],
-    'height' => $size[1],
-  ];
-
-  return array_map(function($ratio) use ($cloud, $size, $img_data) {
-    $url = Cumulus\retina_url(
-      $cloud,
-      $size,
-      $img_data,
-      $ratio
-    );
-
-    return [
-      'url'        => $url,
-      'descriptor' => 'x',
-      'value'      => $ratio,
+    $cloud = Cumulus\cloud_name();
+    $size  = [
+        'width'  => $size[0],
+        'height' => $size[1],
     ];
-  }, $ratios);
+
+    return array_map(function ($ratio) use ($cloud, $size, $img_data) {
+        $url = Cumulus\retina_url(
+            $cloud,
+            $size,
+            $img_data,
+            $ratio
+        );
+
+        return [
+            'url'        => $url,
+            'descriptor' => 'x',
+            'value'      => $ratio,
+        ];
+    }, $ratios);
 }, 10, 5);
 
 /**
  * Include cumulus_image in default attachmenta metadata.
  */
-add_filter('wp_get_attachment_metadata', function($meta, $id) {
-  if (!is_array($meta)) {
+add_filter('wp_get_attachment_metadata', function ($meta, $id) {
+    if (!is_array($meta)) {
+        return $meta;
+    }
+
+    $meta['cumulus_image'] = get_post_meta($id, 'cumulus_image', true) ?: [];
+
     return $meta;
-  }
-
-  $meta['cumulus_image'] = get_post_meta($id, 'cumulus_image', true) ?: [];
-
-  return $meta;
 }, 10, 2);
 
+add_action('rest_api_init', function () {
+    /**
+     * When the Attachment Detail modal opens, we need to fetch additional
+     * data for the attachment being cropped. To do that, we call this endpoint.
+     */
+    register_rest_route('cumulus/v1', '/attachment/(?P<id>\d++)', [
+        'methods'  => 'GET',
+        'callback' => function ($req) {
+            $id = $req->get_param('id');
 
-add_action('rest_api_init', function() {
-  /**
-   * When the Attachment Detail modal opens, we need to fetch additional
-   * data for the attachment being cropped. To do that, we call this endpoint.
-   */
-  register_rest_route('cumulus/v1', '/attachment/(?P<id>\d++)', [
-    'methods'  => 'GET',
-    'callback' => function($req) {
-      $id = $req->get_param('id');
+            $meta = get_post_meta($id, 'cumulus_image', true);
 
-      $meta = get_post_meta($id, 'cumulus_image', true);
+            if (!$meta) {
+                return [
+                    'attachment_id' => $id,
+                    'uploaded'      => false,
+                ];
+            }
 
-      if (!$meta) {
-        return [
-          'attachment_id' => $id,
-          'uploaded'      => false,
-        ];
-      }
+            $cloudinaryData = $meta['cloudinary_data'];
 
-      $cloudinaryData = $meta['cloudinary_data'];
+            return [
+                'attachment_id'  => $id,
+                'uploaded'       => true,
+                'nonce'          => wp_create_nonce("cumulus_attachment_$id"),
+                'version'        => $cloudinaryData['version'],
+                // NOTE: public_id also includes the folder to which the image was uploaded, if any.
+                'filename'       => $cloudinaryData['public_id'] . '.' . $cloudinaryData['format'],
+                'full_url'       => $meta['cloudinary_data']['secure_url'],
+                'full_width'     => $meta['cloudinary_data']['width'],
+                'full_height'    => $meta['cloudinary_data']['height'],
+                // `params_by_size.my_image_size` contains the saved transform params for a given crop
+                'params_by_size' => $meta['params_by_size'] ?? [],
+                'urls_by_size'   => $meta['urls_by_size'] ?? [],
+                // NOTE: global sizes come through via wp_localize_script
+            ];
+        },
+    ]);
 
-      return [
-        'attachment_id'  => $id,
-        'uploaded'       => true,
-        'nonce'          => wp_create_nonce("cumulus_attachment_$id"),
-        'version'        => $cloudinaryData['version'],
-        // NOTE: public_id also includes the folder to which the image was uploaded, if any.
-        'filename'       => $cloudinaryData['public_id'] . '.' . $cloudinaryData['format'],
-        'full_url'       => $meta['cloudinary_data']['secure_url'],
-        'full_width'     => $meta['cloudinary_data']['width'],
-        'full_height'    => $meta['cloudinary_data']['height'],
-        // `params_by_size.my_image_size` contains the saved transform params for a given crop
-        'params_by_size' => $meta['params_by_size'] ?? [],
-        'urls_by_size'   => $meta['urls_by_size'] ?? [],
-        // NOTE: global sizes come through via wp_localize_script
-      ];
-    },
-  ]);
+    /**
+     * Saving changes in the Customize Image Crops modal sends a POST request
+     * to this endpoint.
+     */
+    register_rest_route('cumulus/v1', '/attachment/(?P<id>\d++)', [
+        'methods'  => 'POST',
+        'callback' => function ($req) {
+            $id    = $req->get_param('id');
+            $nonce = $req->get_header('x-nonce');
 
-  /**
-   * Saving changes in the Customize Image Crops modal sends a POST request
-   * to this endpoint.
-   */
-  register_rest_route('cumulus/v1', '/attachment/(?P<id>\d++)', [
-    'methods'  => 'POST',
-    'callback' => function($req) {
-      $id    = $req->get_param('id');
-      $nonce = $req->get_header('x-nonce');
+            if (!wp_verify_nonce($nonce, "cumulus_attachment_$id")) {
+                return [
+                    'success' => false,
+                ];
+            }
 
-      if (!wp_verify_nonce($nonce, "cumulus_attachment_$id")) {
-        return [
-          'success' => false,
-        ];
-      }
+            $meta   = get_post_meta($id, 'cumulus_image', true) ?: [];
+            $config = array_merge($meta, $req->get_json_params());
 
-      $meta   = get_post_meta($id, 'cumulus_image', true) ?: [];
-      $config = array_merge($meta, $req->get_json_params());
+            update_post_meta($id, 'cumulus_image', array_merge($meta, $config));
 
-      update_post_meta($id, 'cumulus_image', array_merge($meta, $config));
-
-      return [
-        'success'        => true,
-        'cumulus_config' => $config,
-      ];
-    },
-  ]);
+            return [
+                'success'        => true,
+                'cumulus_config' => $config,
+            ];
+        },
+    ]);
 });
-
 
 /**
  * Upload to Cloudinary backend when a new Attachment is added.
  */
-add_action('add_attachment', function(int $id) {
-  if (Cumulus\should_upload($id)) {
-    Cumulus\upload_attachment($id);
-  }
+add_action('add_attachment', function (int $id) {
+    if (Cumulus\should_upload($id)) {
+        Cumulus\upload_attachment($id);
+    }
 }, 10);
 
 /**
  * Dynamically set Cloudinary upload params when a new Attachment is uploaded.
  */
-add_filter('cumulus/upload_options', function(array $options) {
-  // TODO Deal with extensions in a less hacky way
-  $extensions = ['/\.jpeg$/', '/\.jpg$/', '/\.png$/'];
-  foreach ($extensions as $pattern) {
-    $options['public_id'] = preg_replace($pattern, '', $options['public_id']);
-  }
+add_filter('cumulus/upload_options', function (array $options) {
+    // TODO Deal with extensions in a less hacky way
+    $extensions = ['/\.jpeg$/', '/\.jpg$/', '/\.png$/'];
+    foreach ($extensions as $pattern) {
+        $options['public_id'] = preg_replace($pattern, '', $options['public_id']);
+    }
 
-  $folder = Cumulus\folder();
-  if ($folder) {
-    $options['public_id'] = $options['public_id'];
-    $options['folder']    = $folder;
-  }
+    $folder = Cumulus\folder();
+    if ($folder) {
+        $options['public_id'] = $options['public_id'];
+        $options['folder']    = $folder;
+    }
 
-  return $options;
+    return $options;
 });
 
 /**
  * Generic filter for rendering content partials.
  */
-add_action('cumulus/render', function($file, $data) {
-  ob_start();
-  include CUMULUS_VIEW_DIR . $file;
-  return ob_get_clean();
+add_action('cumulus/render', function ($file, $data) {
+    ob_start();
+    include CUMULUS_VIEW_DIR . $file;
+    return ob_get_clean();
 }, 10, 2);
 
 /**
  * Hook into the WordPress attachment media modal and inject the code we
  * need on the frontend to bootstrap the Crop UI.
  */
-add_action('print_media_templates', function() {
-  echo apply_filters('cumulus/render', 'media-templates.php', [
-    'customize_crops' => __('Customize Image Crops'),
-    'back_to_details' => __('Back to Attachment Details'),
-  ]);
+add_action('print_media_templates', function () {
+    echo apply_filters('cumulus/render', 'media-templates.php', [
+        'customize_crops' => __('Customize Image Crops'),
+        'back_to_details' => __('Back to Attachment Details'),
+    ]);
 });
 
 /**
  * Enqueue Crop UI JS.
  */
-add_action('admin_enqueue_scripts', function() {
-  $file = CUMULUS_JS_DIR . 'main.js';
-  wp_enqueue_script(
-    'cumulus-crop-ui-js',
-    plugin_dir_url(__FILE__) . 'dist/js/main.js',
-    [],
-    filemtime($file)
-  );
+add_action('admin_enqueue_scripts', function () {
+    $file = CUMULUS_JS_DIR . 'main.js';
+    wp_enqueue_script(
+        'cumulus-crop-ui-js',
+        plugin_dir_url(__FILE__) . 'dist/js/main.js',
+        [],
+        filemtime($file)
+    );
 
-  $registeredSizes = wp_get_registered_image_subsizes();
-  $supportedSizes  = array_intersect_key(
-    $registeredSizes,
-    array_flip(apply_filters('cumulus/sizes', array_keys($registeredSizes)))
-  );
+    $registeredSizes = wp_get_registered_image_subsizes();
+    $supportedSizes  = array_intersect_key(
+        $registeredSizes,
+        array_flip(apply_filters('cumulus/sizes', array_keys($registeredSizes)))
+    );
 
-  $sizes = array_map(function($key, $dimensions) {
-    return [
-      'size_name' => $key,
-      'width'     => $dimensions['width'],
-      'height'    => $dimensions['height'],
-      'hard'      => $dimensions['crop'],
-    ];
-  }, array_keys($supportedSizes), array_values($supportedSizes));
+    $sizes = array_map(function ($key, $dimensions) {
+        return [
+            'size_name' => $key,
+            'width'     => $dimensions['width'],
+            'height'    => $dimensions['height'],
+            'hard'      => $dimensions['crop'],
+        ];
+    }, array_keys($supportedSizes), array_values($supportedSizes));
 
-  // This is information we know completely ahead of time, before the front-end
-  // loads any attachment-specific data.
-  wp_localize_script('cumulus-crop-ui-js', 'CUMULUS_CONFIG', [
-    // TODO remove in favor of "cloud"
-    'bucket'   => Cumulus\cloud_name(),
-    'cloud'    => Cumulus\cloud_name(),
-    'sizes'    => $sizes,
-    'WP_DEBUG' => WP_DEBUG,
-  ]);
+    // This is information we know completely ahead of time, before the front-end
+    // loads any attachment-specific data.
+    wp_localize_script('cumulus-crop-ui-js', 'CUMULUS_CONFIG', [
+        // TODO remove in favor of "cloud"
+        'bucket'   => Cumulus\cloud_name(),
+        'cloud'    => Cumulus\cloud_name(),
+        'sizes'    => $sizes,
+        'WP_DEBUG' => WP_DEBUG,
+    ]);
 
-  wp_enqueue_style('cropper-css', plugin_dir_url(__FILE__) . 'dist/css/react-image-crop.css', [], '1.5.7');
+    wp_enqueue_style('cropper-css', plugin_dir_url(__FILE__) . 'dist/css/react-image-crop.css', [], '1.5.7');
 
-  $css = CUMULUS_CSS_DIR . 'main.css';
-  wp_enqueue_style('cumulus-css', plugin_dir_url(__FILE__) . 'dist/css/main.css', ['cropper-css'], filemtime($css));
+    $css = CUMULUS_CSS_DIR . 'main.css';
+    wp_enqueue_style('cumulus-css', plugin_dir_url(__FILE__) . 'dist/css/main.css', ['cropper-css'], filemtime($css));
 });
 
 if (defined('WP_CLI') && WP_CLI) {
-  require_once __DIR__ . '/wp-cli.php';
-  WP_CLI::add_command('cumulus bulk-upload', WpCli\bulk_upload::class);
-  WP_CLI::add_command('cumulus regenerate', WpCli\regenerate::class);
+    require_once __DIR__ . '/wp-cli.php';
+    WP_CLI::add_command('cumulus bulk-upload', WpCli\bulk_upload::class);
+    WP_CLI::add_command('cumulus regenerate', WpCli\regenerate::class);
 }
